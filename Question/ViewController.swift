a7ad73085cea0cbdd5017ec7563b4ef60aa8b375
bowen163 <763893371@qq.com>
2019-11-27 10:26:34 +0800

    Initial Commit

---
+++ Question/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Question
+//
+//  Created by s20181102936 on 2019/11/27.
+//  Copyright © 2019 s20181102936. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
